de78a8991fdb18aca50ef05636b8adcac5178321
zcjia <zcjia@suse.com>
2021-05-25 14:58:32 +0800

    try mouse_drag for gnome

--- lib/x11utils.pm
+++ lib/x11utils.pm
@@ -198,7 +198,7 @@ sub handle_login {
 
     save_screenshot();
     wait_still_screen();
-    mouse_set(0, 0);
+    mouse_drag(startx => 100, starty => 100, endx => 0, endy => 0);
     # wait for DM, avoid screensaver and try to login
     # Previously this pressed esc, but that makes the text field in SDDM lose focus
     send_key_until_needlematch('displaymanager', 'shift', 30, 3);
